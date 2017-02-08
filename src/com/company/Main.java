431d7732602eae3019d0e6fb8af8c01f716784ef
Aleksey Gavrusev <lexa1907_98@mail.ru>
2017-02-08 20:29:06 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //TODO Display "Hello World!"
+        System.out.println("Hello World! Oleg");
+        System.out.println("Hello World! Oleg");
+    }
+}
